c2a806896c81fe30e19fcac7f290ec69efa795aa
Svein-Tore Griff With <s.t.g.with@gmail.com>
2014-10-13 21:53:29 +0200

    Added xAPI compatibility

--- iframe-embed.js
+++ iframe-embed.js
@@ -50,7 +50,7 @@ H5P.IFrameEmbed = function (options, contentId) {
       }, 1);
     }
     
-    this.$.trigger('resize');
+    this.triggerH5PEvent('resize');
   };
 
   this.resize = function () {
